35969f42805252587ecf6ed52d7bad3feaf2e991
andre-gomes-oliveira <andre.gomes.olivera@gmail.com>
2017-11-04 13:00:25 -0200

    Lesson 11 - Sunshine exercises completed

--- S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncTask.java
+++ S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncTask.java
@@ -1,4 +1,51 @@
-//  TODO (1) Create a class called SunshineSyncTask
-//  TODO (2) Within SunshineSyncTask, create a synchronized public static void method called syncWeather
-//      TODO (3) Within syncWeather, fetch new weather data
-//      TODO (4) If we have valid results, delete the old data and insert the new
+package com.example.android.sunshine.sync;
+
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
+import android.util.Log;
+
+import com.example.android.sunshine.data.WeatherContract;
+import com.example.android.sunshine.utilities.NetworkUtils;
+import com.example.android.sunshine.utilities.OpenWeatherJsonUtils;
+
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.net.URL;
+
+//  COMPLETED (1) Create a class called SunshineSyncTask
+//  COMPLETED (2) Within SunshineSyncTask, create a synchronized public static void method called syncWeather
+//      COMPLETED (3) Within syncWeather, fetch new weather data
+//      COMPLETED (4) If we have valid results, delete the old data and insert the new
+public class SunshineSyncTask {
+    private final static String TAG = SunshineSyncTask.class.getSimpleName();
+
+    synchronized public static void  syncWeather (Context context){
+        try {
+            URL weatherRequestUrl = NetworkUtils.getUrl(context);
+
+            String jsonWeatherResponse = NetworkUtils.getResponseFromHttpUrl(weatherRequestUrl);
+
+            ContentValues[] weatherValues = OpenWeatherJsonUtils
+                    .getWeatherContentValuesFromJson(context, jsonWeatherResponse);
+
+            if(weatherValues != null && weatherValues.length > 0){
+                ContentResolver resolver = context.getContentResolver();
+
+                resolver.delete(
+                        WeatherContract.WeatherEntry.CONTENT_URI,
+                        null,
+                        null);
+
+                resolver.bulkInsert(WeatherContract.WeatherEntry.CONTENT_URI,
+                        weatherValues);
+            }
+        }
+        catch (JSONException e) {
+            Log.e(TAG, "syncWeather: There are problems with the JSON file.", e);
+        } catch (IOException e) {
+            Log.e(TAG, "syncWeather: IO error.", e);
+        }
+    }
+}
--- S10.04-Exercise-Notifications/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncTask.java
+++ S10.04-Exercise-Notifications/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncTask.java
@@ -18,9 +18,15 @@ package com.example.android.sunshine.sync;
 import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.text.format.DateUtils;
 
+import com.example.android.sunshine.R;
+import com.example.android.sunshine.data.SunshinePreferences;
 import com.example.android.sunshine.data.WeatherContract;
 import com.example.android.sunshine.utilities.NetworkUtils;
+import com.example.android.sunshine.utilities.NotificationUtils;
 import com.example.android.sunshine.utilities.OpenWeatherJsonUtils;
 
 import java.net.URL;
@@ -73,11 +79,32 @@ public class SunshineSyncTask {
                         WeatherContract.WeatherEntry.CONTENT_URI,
                         weatherValues);
 
-//              TODO (13) Check if notifications are enabled
+//              COMPLETED (13) Check if notifications are enabled
+                boolean shouldDisplayNotificationsByDefault = context
+                        .getResources()
+                        .getBoolean(R.bool.show_notifications);
 
-//              TODO (14) Check if a day has passed since the last notification
+                SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
 
-//              TODO (15) If more than a day have passed and notifications are enabled, notify the user
+                boolean shouldDisplayNotifications = preferences
+                        .getBoolean(context.getString(R.string.pref_enable_notifications_key),
+                                shouldDisplayNotificationsByDefault);
+
+//              COMPLETED (14) Check if a day has passed since the last notification
+                long timeSinceLastNotification = SunshinePreferences
+                        .getEllapsedTimeSinceLastNotification(context);
+
+                boolean oneDayPassedSinceLastNotification = false;
+
+//              COMPLETED (14) Check if a day has passed since the last notification
+                if (timeSinceLastNotification >= DateUtils.DAY_IN_MILLIS) {
+                    oneDayPassedSinceLastNotification = true;
+                }
+
+//              COMPLETED (15) If more than a day have passed and notifications are enabled, notify the user
+                if (shouldDisplayNotifications && oneDayPassedSinceLastNotification) {
+                    NotificationUtils.notifyUserOfNewWeather(context);
+                }
 
             /* If the code reaches this point, we have successfully performed our sync */
 
